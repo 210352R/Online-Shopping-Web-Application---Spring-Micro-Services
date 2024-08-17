b756af3fda33e0e56014637eb59c5b1ae6f00960
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-17 07:54:00 +0530

    add product service methods

--- product-service/product-service/src/main/java/com/eshan/microservices/product/service/ProductService.java
+++ product-service/product-service/src/main/java/com/eshan/microservices/product/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.eshan.microservices.product.service;
 
 import com.eshan.microservices.product.dto.ProductRequest;
+import com.eshan.microservices.product.dto.ProductResponse;
 import com.eshan.microservices.product.model.Product;
 import com.eshan.microservices.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,7 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public void createProduct(ProductRequest productRequest){
+    public ProductResponse createProduct(ProductRequest productRequest){
         Product product = Product.builder()
                 .id(productRequest.id())
                 .name(productRequest.name())
@@ -22,6 +25,17 @@ public class ProductService {
                 .build();
         productRepository.save(product);
         log.info("Product created sucessfully with id: {}", product.getId());
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public ProductResponse getProduct(String id){
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice())).toList();
     }
 
 
