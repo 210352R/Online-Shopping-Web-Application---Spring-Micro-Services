85c5b84b35c30c94efabb6bb082d7c9f1f612b1a
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-17 08:52:41 +0530

    add controllers

--- product-service/product-service/src/main/java/com/eshan/microservices/product/controller/ProductController.java
+++ product-service/product-service/src/main/java/com/eshan/microservices/product/controller/ProductController.java
@@ -1,16 +1,33 @@
 package com.eshan.microservices.product.controller;
 
 import com.eshan.microservices.product.dto.ProductRequest;
+import com.eshan.microservices.product.dto.ProductResponse;
+import com.eshan.microservices.product.service.ProductService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductRequest productRequest){
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest){
+        System.out.println("Product Request: -------------------"+productRequest);
+        return productService.createProduct(productRequest);
 
+    }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
