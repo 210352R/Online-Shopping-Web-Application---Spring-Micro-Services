204e00fcc58b431fb811281e43694fb9ce53d761
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-17 09:21:10 +0530

    add unit test for product creation

--- product-service/product-service/src/test/java/com/eshan/microservices/product/ProductServiceApplicationTests.java
+++ product-service/product-service/src/test/java/com/eshan/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package com.eshan.microservices.product;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+import static org.hamcrest.Matchers.equalTo;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+
+	}
 
 	@Test
-	void contextLoads() {
+	void shouldProductCreate() {
+		String requestBody = """
+    {
+				"name": "Example Product",
+				        "description": "This is an example product description.",
+				        "price": 99.99
+    }
+				""";
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", org.hamcrest.Matchers.notNullValue())
+				.body("name", equalTo("Example Product"))
+				.body("description", equalTo("This is an example product description."))
+				.body("price", equalTo(99.99f));
+
 	}
 
 }
