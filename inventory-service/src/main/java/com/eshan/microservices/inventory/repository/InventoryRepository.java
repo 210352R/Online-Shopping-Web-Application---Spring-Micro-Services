4cc275eace23bd695754ff8a6590f2b7d3a036b3
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-18 17:44:14 +0530

    add inventory functioons and their controllers

---
+++ inventory-service/src/main/java/com/eshan/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.eshan.microservices.inventory.repository;
+
+import com.eshan.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
